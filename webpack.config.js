79de902b0b17f0aca397f494398ff8d82ec4bb62
yato <2602231824@qq.com>
2020-01-22 17:36:59 +0800

    管理输出

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print:'./src/print.js'
+    },
+    plugins:[
+        // 新版本，此处不用传递数组，默认清空output的path文件夹
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[hash:8].bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
